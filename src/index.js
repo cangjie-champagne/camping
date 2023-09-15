168ed6e67548115245bdd3a597fbccedbfcb8539
João Pedro <joaopjt@icloud.com>
2023-09-15 13:13:17 -0300

    added d() and e() method values

--- src/index.js
+++ src/index.js
@@ -220,7 +220,7 @@ class Camping {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['disaster'],
       web: []
     };
     
@@ -236,19 +236,19 @@ class Camping {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['gold'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['big'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['one'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['dagger-axe'];
           break;
       }
     }
