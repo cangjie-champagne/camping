799304a754444f7e7806b37ac3079d190a38c597
João Pedro <joaopjt@icloud.com>
2023-09-15 13:16:06 -0300

    added f() method values

--- src/index.js
+++ src/index.js
@@ -300,7 +300,7 @@ class Camping {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['hand'],
       web: []
     };
     
@@ -316,19 +316,19 @@ class Camping {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['ten'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['heart'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['bow'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['predict'];
           break;
       }
     }
