a8cb13c5b965afb3888d7ee4001abd4d1f6138d5
João Pedro <joaopjt@icloud.com>
2023-09-15 13:28:05 -0300

    added j() method values

--- src/index.js
+++ src/index.js
@@ -456,7 +456,7 @@ class Camping {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['field'],
       web: []
     };
     
@@ -472,19 +472,19 @@ class Camping {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['bamboo'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['people'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['disaster'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['gold'];
           break;
       }
     }
